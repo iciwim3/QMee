71198342e8b225d7500ef2816abcc166b8c9768a
SKE <appybuildmore@gmail.com>
2017-08-06 08:20:22 -0500

    1. Created a GameViewController. 2. Made the View on the Storyboard the GameViewController.

---
+++ QMee/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  QMee
+//
+//  Created by Sain-R Edwards Jr. on 8/6/17.
+//  Copyright © 2017 Appybuildmore. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("The Game View Controller just loaded!")
+    }
+    
+}
